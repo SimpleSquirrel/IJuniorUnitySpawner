c434bfe4539fbc729f37f0e4e55b4bf339b96134
Simple Squirrel <simplesquirrel9071@gmail.com>
2020-08-29 20:12:04 +0300

    first try

---
+++ Assets/Scripts/RandomizeSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomizeSpawn : MonoBehaviour
+{
+    private List<SpawnDrop> _spawners;
+    [SerializeField] private float _spawnCooldownTime;
+    private float _cooldownTime;
+    public void Start()
+    {
+        _spawners = FindObjectsOfType<SpawnDrop>().ToList();
+    }
+    public void Update()
+    {
+        if (_cooldownTime >= _spawnCooldownTime)
+        {
+            int choosenSpawner = Random.Range(0, _spawners.Count);
+            _spawners[choosenSpawner].SpawnNewDrop();
+            _cooldownTime = 0;
+        }
+        else
+        {
+            _cooldownTime += Time.deltaTime;
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/SpawnDrop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDrop : MonoBehaviour
+{
+    public GameObject Template;
+
+    public void SpawnNewDrop()
+    {
+        var spawnCoordinates = GetComponent<Transform>();
+        Instantiate(Template, new Vector3(spawnCoordinates.position.x, spawnCoordinates.position.y, 0), Quaternion.identity);
+    }
+}
